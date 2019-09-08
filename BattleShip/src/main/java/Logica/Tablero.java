18f5cd92cdd36880b8be453fc83b946ced61e8ee
Nico <nicoinde@gmail.com>
2019-09-08 04:12:10 -0300

    Tablero ahora carga bien los barcos

--- BattleShip/src/main/java/Logica/Tablero.java
+++ BattleShip/src/main/java/Logica/Tablero.java
@@ -34,7 +34,7 @@ public class Tablero {
         boolean ok = true;
         int x = pos[0];
         int y = pos[1];
-        
+
         //CONTROL PARA BARCOS HORIZONTALES
         if (horizontal && (y + tamaño < TAMAÑO_TABLERO)) {
             int opt = 0;
@@ -54,15 +54,15 @@ public class Tablero {
                         }
                     }
                     if (y + tamaño < TAMAÑO_TABLERO - 1) {
-                        String aux1 = tablero[x][y+tamaño].getActual().toString();
-                        String aux2 = tablero[x + 1][y+tamaño].getActual().toString();
+                        String aux1 = tablero[x][y + tamaño].getActual().toString();
+                        String aux2 = tablero[x + 1][y + tamaño].getActual().toString();
                         if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux2 == "AguaForzosa") {
                             return false;
                         }
                     }
                     if (y > 0) {
-                        String aux1 = tablero[x][y-1].getActual().toString();
-                        String aux2 = tablero[x + 1][y-1].getActual().toString();
+                        String aux1 = tablero[x][y - 1].getActual().toString();
+                        String aux2 = tablero[x + 1][y - 1].getActual().toString();
                         if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux2 == "AguaForzosa") {
                             return false;
                         }
@@ -78,18 +78,18 @@ public class Tablero {
                         }
                     }
                     if (y + tamaño < TAMAÑO_TABLERO - 1) {
-                        String aux0 = tablero[x - 1][y+tamaño].getActual().toString();
-                        String aux1 = tablero[x][y+tamaño].getActual().toString();
-                        String aux2 = tablero[x + 1][y+tamaño].getActual().toString();
-                        if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa" || aux2 == "AguaForzosa" || aux0 == "Barco" ) {
+                        String aux0 = tablero[x - 1][y + tamaño].getActual().toString();
+                        String aux1 = tablero[x][y + tamaño].getActual().toString();
+                        String aux2 = tablero[x + 1][y + tamaño].getActual().toString();
+                        if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa" || aux2 == "AguaForzosa" || aux0 == "Barco") {
                             return false;
                         }
                     }
                     if (y > 0) {
-                        String aux0 = tablero[x - 1][y-1].getActual().toString();
-                        String aux1 = tablero[x][y-1].getActual().toString();
-                        String aux2 = tablero[x + 1][y-1].getActual().toString();
-                        if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa" || aux2 == "AguaForzosa" || aux0 == "Barco" ) {
+                        String aux0 = tablero[x - 1][y - 1].getActual().toString();
+                        String aux1 = tablero[x][y - 1].getActual().toString();
+                        String aux2 = tablero[x + 1][y - 1].getActual().toString();
+                        if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa" || aux2 == "AguaForzosa" || aux0 == "Barco") {
                             return false;
                         }
                     }
@@ -103,26 +103,25 @@ public class Tablero {
                         }
                     }
                     if (y + tamaño < TAMAÑO_TABLERO - 1) {
-                        String aux0 = tablero[x - 1][y+tamaño].getActual().toString();
-                        String aux1 = tablero[x][y+tamaño].getActual().toString();
+                        String aux0 = tablero[x - 1][y + tamaño].getActual().toString();
+                        String aux1 = tablero[x][y + tamaño].getActual().toString();
                         if (aux1 == "Barco" || aux0 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa") {
                             return false;
                         }
                     }
                     if (y > 0) {
-                        String aux0 = tablero[x - 1][y-1].getActual().toString();
-                        String aux1 = tablero[x][y-1].getActual().toString();
+                        String aux0 = tablero[x - 1][y - 1].getActual().toString();
+                        String aux1 = tablero[x][y - 1].getActual().toString();
                         if (aux1 == "Barco" || aux0 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa") {
                             return false;
                         }
                     }
                     break;
             }
-            
+
         }
-        
+
         //CONTROL PARA BARCOS VERTICALES
-        
         if (!horizontal && (x + tamaño < TAMAÑO_TABLERO)) {
             int opt = 0;
             if (y != 0 && y < TAMAÑO_TABLERO - 1) {
@@ -135,21 +134,21 @@ public class Tablero {
                 case 0:
                     for (int i = x; i < x + tamaño; i++) {
                         String aux1 = tablero[i][y].getActual().toString();
-                        String aux2 = tablero[i][y+1].getActual().toString();
+                        String aux2 = tablero[i][y + 1].getActual().toString();
                         if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa") {
                             return false;
                         }
                     }
                     if (x + tamaño < TAMAÑO_TABLERO - 1) {
-                        String aux1 = tablero[x+tamaño][y].getActual().toString();
-                        String aux2 = tablero[x+tamaño][y+1].getActual().toString();
+                        String aux1 = tablero[x + tamaño][y].getActual().toString();
+                        String aux2 = tablero[x + tamaño][y + 1].getActual().toString();
                         if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux2 == "AguaForzosa") {
                             return false;
                         }
                     }
                     if (x > 0) {
-                        String aux1 = tablero[x-1][y].getActual().toString();
-                        String aux2 = tablero[x - 1][y+1].getActual().toString();
+                        String aux1 = tablero[x - 1][y].getActual().toString();
+                        String aux2 = tablero[x - 1][y + 1].getActual().toString();
                         if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux2 == "AguaForzosa") {
                             return false;
                         }
@@ -159,61 +158,176 @@ public class Tablero {
                     for (int i = x; i < x + tamaño; i++) {
                         String aux0 = tablero[i][y - 1].getActual().toString();
                         String aux1 = tablero[i][y].getActual().toString();
-                        String aux2 = tablero[i][y+1].getActual().toString();
+                        String aux2 = tablero[i][y + 1].getActual().toString();
                         if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux0 == "Barco") {
                             return false;
                         }
                     }
                     if (x + tamaño < TAMAÑO_TABLERO - 1) {
-                        String aux0 = tablero[x+tamaño][y-1].getActual().toString();
-                        String aux1 = tablero[x+tamaño][y].getActual().toString();
-                        String aux2 = tablero[x+tamaño][y+1].getActual().toString();
-                        if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa" || aux2 == "AguaForzosa" || aux0 == "Barco" ) {
+                        String aux0 = tablero[x + tamaño][y - 1].getActual().toString();
+                        String aux1 = tablero[x + tamaño][y].getActual().toString();
+                        String aux2 = tablero[x + tamaño][y + 1].getActual().toString();
+                        if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa" || aux2 == "AguaForzosa" || aux0 == "Barco") {
                             return false;
                         }
                     }
                     if (x > 0) {
-                        String aux0 = tablero[x - 1][y-1].getActual().toString();
-                        String aux1 = tablero[x-1][y].getActual().toString();
-                        String aux2 = tablero[x - 1][y+1].getActual().toString();
-                        if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa" || aux2 == "AguaForzosa" || aux0 == "Barco" ) {
+                        String aux0 = tablero[x - 1][y - 1].getActual().toString();
+                        String aux1 = tablero[x - 1][y].getActual().toString();
+                        String aux2 = tablero[x - 1][y + 1].getActual().toString();
+                        if (aux1 == "Barco" || aux2 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa" || aux2 == "AguaForzosa" || aux0 == "Barco") {
                             return false;
                         }
                     }
                     break;
                 case 2:
                     for (int i = x; i < x + tamaño; i++) {
-                        String aux0 = tablero[i][y-1].getActual().toString();
+                        String aux0 = tablero[i][y - 1].getActual().toString();
                         String aux1 = tablero[i][y].getActual().toString();
                         if (aux1 == "Barco" || aux1 == "AguaForzosa" || aux0 == "Barco") {
                             return false;
                         }
                     }
                     if (x + tamaño < TAMAÑO_TABLERO - 1) {
-                        String aux0 = tablero[x+tamaño][y-1].getActual().toString();
-                        String aux1 = tablero[x+tamaño][y].getActual().toString();
+                        String aux0 = tablero[x + tamaño][y - 1].getActual().toString();
+                        String aux1 = tablero[x + tamaño][y].getActual().toString();
                         if (aux1 == "Barco" || aux0 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa") {
                             return false;
                         }
                     }
                     if (x > 0) {
-                        String aux0 = tablero[x - 1][y-1].getActual().toString();
-                        String aux1 = tablero[x-1][y].getActual().toString();
+                        String aux0 = tablero[x - 1][y - 1].getActual().toString();
+                        String aux1 = tablero[x - 1][y].getActual().toString();
                         if (aux1 == "Barco" || aux0 == "Barco" || aux1 == "AguaForzosa" || aux0 == "AguaForzosa") {
                             return false;
                         }
                     }
                     break;
             }
-            
+
         }
         return ok;
     }
 
     public boolean ubicarBarco(Barco bote, int[] pos) {
         boolean ok = true;
+        int x = pos[0];
+        int y = pos[1];
+        int tamaño = bote.getTamaño();
+        if (!hayEspacio(pos, bote.getTamaño(), bote.isHorizontal())) {
+            return false;
+        }
         if (bote.isHorizontal()) {
-
+            int opt = 0;
+            if (x != 0 && x < TAMAÑO_TABLERO - 1) {
+                opt = 1;
+            }
+            if (x == TAMAÑO_TABLERO - 1) {
+                opt = 2;
+            }
+            switch (opt) {
+                case 0:
+                    for (int i = y; i < y + tamaño; i++) {
+                        tablero[x][i].setBarco();
+                        tablero[x + 1][i].setForzosa();
+                    }
+                    if (y + tamaño < TAMAÑO_TABLERO - 1) {
+                        tablero[x][y + tamaño].setForzosa();
+                        tablero[x + 1][y + tamaño].setForzosa();
+                    }
+                    if (y > 0) {
+                        tablero[x][y - 1].setForzosa();
+                        tablero[x + 1][y - 1].setForzosa();
+                    }
+                    break;
+                case 1:
+                    for (int i = y; i < y + tamaño; i++) {
+                        tablero[x][i].setBarco();
+                        tablero[x + 1][i].setForzosa();
+                        tablero[x - 1][i].setForzosa();
+                    }
+                    if (y + tamaño < TAMAÑO_TABLERO - 1) {
+                        tablero[x][y + tamaño].setForzosa();
+                        tablero[x + 1][y + tamaño].setForzosa();
+                        tablero[x - 1][y + tamaño].setForzosa();
+                    }
+                    if (y > 0) {
+                        tablero[x][y - 1].setForzosa();
+                        tablero[x + 1][y - 1].setForzosa();
+                        tablero[x - 1][y - 1].setForzosa();
+                    }
+                    break;
+                case 2:
+                    for (int i = y; i < y + tamaño; i++) {
+                        tablero[x][i].setBarco();
+                        tablero[x - 1][i].setForzosa();
+                    }
+                    if (y + tamaño < TAMAÑO_TABLERO - 1) {
+                        tablero[x][y + tamaño].setForzosa();
+                        tablero[x - 1][y + tamaño].setForzosa();
+                    }
+                    if (y > 0) {
+                        tablero[x][y - 1].setForzosa();
+                        tablero[x - 1][y - 1].setForzosa();
+                    }
+                    break;
+            }
+            
+        } else { //Ponemos el barco Vertical
+            int opt = 0;
+            if (y != 0 && y < TAMAÑO_TABLERO - 1) {
+                opt = 1;
+            }
+            if (y == TAMAÑO_TABLERO - 1) {
+                opt = 2;
+            }
+            switch (opt) {
+                case 0:
+                    for (int i = x; i < x + tamaño; i++) {
+                        tablero[i][y].setBarco();
+                        tablero[i][y+1].setForzosa();
+                    }
+                    if (x + tamaño < TAMAÑO_TABLERO - 1) {
+                        tablero[x + tamaño][y].setForzosa();
+                        tablero[x + tamaño][y + 1].setForzosa();
+                    }
+                    if (x > 0) {
+                        tablero[x-1][y].setForzosa();
+                        tablero[x - 1][y + 1].setForzosa();
+                    }
+                    break;
+                case 1:
+                    for (int i = x; i < x + tamaño; i++) {
+                        tablero[i][y].setBarco();
+                        tablero[i][y+1].setForzosa();
+                        tablero[i][y-1].setForzosa();
+                    }
+                    if (x + tamaño < TAMAÑO_TABLERO - 1) {
+                        tablero[x + tamaño][y].setForzosa();
+                        tablero[x + tamaño][y +1].setForzosa();
+                        tablero[x + tamaño][y -1].setForzosa();
+                    }
+                    if (x > 0) {
+                        tablero[x-1][y].setForzosa();
+                        tablero[x-1][y - 1].setForzosa();
+                        tablero[x - 1][y + 1].setForzosa();
+                    }
+                    break;
+                case 2:
+                    for (int i = x; i < x + tamaño; i++) {
+                        tablero[i][y].setBarco();
+                        tablero[i][y-1].setForzosa();
+                    }
+                    if (x + tamaño < TAMAÑO_TABLERO - 1) {
+                        tablero[x + tamaño][y].setForzosa();
+                        tablero[x + tamaño][y -1].setForzosa();
+                    }
+                    if (y > 0) {
+                        tablero[x-1][y].setForzosa();
+                        tablero[x - 1][y - 1].setForzosa();
+                    }
+                    break;
+            }
         }
         return ok;
     }
